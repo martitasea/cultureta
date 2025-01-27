f67bd31c885bf48b3a75628cd99fc5e0436e60b2
martitasea <marta.pulido.asin@gmail.com>
2025-01-27 09:13:33 +0100

    Añade los puntos al mapa

--- src/views/Map/MainContent.tsx
+++ src/views/Map/MainContent.tsx
@@ -1,17 +1,66 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useMemo, useState} from 'react';
 
 import Map from '@geomatico/geocomponents/Map/Map';
 
 import {INITIAL_VIEWPORT} from '../../config';
+import {CulturalEvent} from '../../domain/entities/CulturalEvent';
+import {toFeatureCollection} from '../../serviceImpl/AytoMappers';
+import { LayerSpecification, SourceSpecification } from 'maplibre-gl';
 
 export type MainContentProps = {
-  mapStyle: string
+  mapStyle: string,
+  culturalEvents: Array<CulturalEvent>
 };
 
-const MainContent: FC<MainContentProps> = ({mapStyle}) => {
+const MainContent: FC<MainContentProps> = ({culturalEvents, mapStyle}) => {
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
 
+  const sources: {
+		[_: string]: SourceSpecification;
+	} = useMemo(() => (
+	  {
+	    culturalEvents: {
+	      type: 'geojson',
+	      data: toFeatureCollection(culturalEvents)
+	    }
+	  }
+	), [culturalEvents]);
+
+  const layers: Array<LayerSpecification> = [
+    {
+      'id': 'culturalEvents-points',
+      'source': 'culturalEvents',
+      'type': 'circle',
+      'paint': {
+        'circle-color': ['get', 'color'],
+        'circle-stroke-width': [
+          'case',
+          ['==', ['get', 'free'], true],
+          1,
+          0
+        ],
+        'circle-stroke-color': '#000000',
+        'circle-radius': ['interpolate', ['linear'], ['zoom'], 14, 5, 20, 2],
+      }
+    },
+    {
+      'id': 'culturalEvents-organizer',
+      'source': 'culturalEvents',
+      'type': 'symbol',
+      'minzoom': 12,
+      'layout': {
+        'text-field':  ['get', 'title'],
+        'text-anchor': 'left',
+        'text-justify': 'left',
+        'text-radial-offset': 1,
+        'text-size': 20
+      }
+    }
+  ];
+
   return <Map
+    sources={sources}
+    layers={layers}
     mapStyle={mapStyle}
     viewport={viewport}
     onViewportChange={setViewport}
